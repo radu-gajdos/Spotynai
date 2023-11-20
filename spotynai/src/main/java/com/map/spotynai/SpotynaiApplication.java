a2d3c7ba3817e2fe1c179ebb548b060e863bcc46
Paul <paul2003mai@yahoo.com>
2023-11-20 14:25:56 -0800

    innebunesc

--- spotynai/src/main/java/com/map/spotynai/SpotynaiApplication.java
+++ spotynai/src/main/java/com/map/spotynai/SpotynaiApplication.java
@@ -1,13 +1,31 @@
 package com.map.spotynai;
 
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @SpringBootApplication
-public class SpotynaiApplication {
+@Log
+public class SpotynaiApplication implements CommandLineRunner {
+
+	private final DataSource dataSource;
+
+	public SpotynaiApplication(final DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpotynaiApplication.class, args);
 	}
 
+	@Override
+	public void run(final String... args){
+		log.info("Datasource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+	}
 }
