509d68dd677c413f2191b54c8236e1a9e967f75d
Paul <paul2003mai@yahoo.com>
2023-11-21 01:18:21 -0800

    stillNotWorking

--- spotynai/src/main/java/com/map/spotynai/SpotynaiApplication.java
+++ spotynai/src/main/java/com/map/spotynai/SpotynaiApplication.java
@@ -1,13 +1,9 @@
 package com.map.spotynai;
 
 import lombok.extern.java.Log;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 
 @SpringBootApplication
 @Log
