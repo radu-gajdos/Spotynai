daf9481284604daded17f070a05e31149513d974
Paul <paul2003mai@yahoo.com>
2023-11-27 20:38:58 +0200

    Changes by Radu

--- spotynai/src/main/java/com/map/SpotynaiApplication.java
+++ spotynai/src/main/java/com/map/SpotynaiApplication.java
@@ -1,5 +1,6 @@
 package com.map;
 
+import com.map.Repositories.ArtistRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
